a067ffbb4bc3a92750c6480f04c6352a16bb71bc
Bence Király <kiralybencepecs@gmail.com>
2022-05-28 14:23:13 +0200

    Added task editing

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,28 +1,56 @@
 <template>
     <li>
-        <span
-            class="task"
-            :class="{ 'strikethrough': task.completed }"
-            @click="toggleTask"
-        >
-            {{ task.description }}
-        </span>
+        <div v-if="!isEditing">
+            <span
+                class="task"
+                :class="{ 'strikethrough': task.completed }"
+                @click="toggleTask"
+            >
+                {{ task.description }}
+            </span>
 
-        <button
-            class="remove-task-button"
-            @click="removeTask"
-        >Remove</button>
+            <button
+                class="task-button"
+                @click="toggleEditor"
+            >Edit</button>
+
+            <button
+                class="task-button"
+                @click="removeTask"
+            >Remove</button>
+        </div>
+
+        <div v-else>
+            <ListItemEditor
+                :task="task"
+                @edit-saved="toggleEditor"
+                @edit-cancelled="toggleEditor"
+            />
+        </div>
     </li>
 </template>
 
 <script>
+import ListItemEditor from './ListItemEditor.vue';
+
 export default {
+    components: {
+        ListItemEditor,
+    },
     props: [
         'task',
     ],
+    data() {
+        return {
+            isEditing: false,
+        };
+    },
     methods: {
         toggleTask() {
             this.task.completed = !this.task.completed;
+        },
+        toggleEditor() {
+            this.isEditing = !this.isEditing;
         },
         removeTask() {
             this.$emit('removeTask', this.task.id);
---
+++ src/components/ListItemEditor.vue
@@ -0,0 +1,36 @@
+<template>
+    <input type="text" v-model="draft">
+
+    <button
+        class="task-button"
+        @click="save"
+    >Save</button>
+
+    <button
+        class="task-button"
+        @click="cancel"
+    >Cancel</button>
+</template>
+
+<script>
+export default {
+    props: [
+        'task',
+    ],
+    data() {
+        return {
+            draft: this.task.description,
+        };
+    },
+    methods: {
+        save() {
+            this.task.description = this.draft;
+            this.$emit('editSaved');
+        },
+        cancel() {
+            this.draft = this.task.description;
+            this.$emit('editCancelled');
+        },
+    },
+}
+</script>
